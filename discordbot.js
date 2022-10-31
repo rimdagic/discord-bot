5f6eeedc352105d6b084753a10650381113548e9
rimdagic <aagic1@hotmail.com>
2022-10-31 14:21:26 +0100

    first commit

---
+++ discordbot.js
@@ -0,0 +1,31 @@
+// Initialize dotenv
+require('dotenv').config();
+
+// Discord.js versions ^13.0 require us to explicitly define client intents
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent
+] 
+});
+
+
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.reply('pong')
+    }
+    if (message.content === '!roll') {
+        let dice = Math.floor(Math.random()*6)+1
+        dice = dice.toString();
+        message.reply(dice)
+    }
+})
+
+// Log In our bot
+client.login(process.env.TOKEN);
